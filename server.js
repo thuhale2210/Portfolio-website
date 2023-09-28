b2c79b01c1c430b5e0902085e122033b2a39aa0f
thuhale2210 <thuhale2210@gmail.com>
2023-09-28 14:33:58 -0400

    Fixing port issue for Heroku deployment

--- server.js
+++ server.js
@@ -6,7 +6,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 var express = require('./config/express')
 var app = express()
-app.listen(3000)
-module.exports = app
+const port = process.env.PORT || 3000;
 
-console.log('Server running at https://localhost:3000/')
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+module.exports = app
